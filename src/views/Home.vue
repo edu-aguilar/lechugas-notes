d2921ca19bd80965726120e8ba9a7c780b34c828
Adrián Fernández <adrianfernandezdiazg@gmail.com>
2019-05-20 11:17:33 +0200

    feat: "created the create user component".

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,21 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CreateUser> </CreateUser>
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import CreateUser from '@/components/CreateUser.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+    CreateUser
   }
 }
 </script>
