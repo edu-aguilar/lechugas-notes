38fa6f6e50aede1c0a4ff167e8f2cdb941621828
Adrián Fernández <adrianfernandezdiazg@gmail.com>
2019-05-22 21:20:24 +0200

    feature: Created the UserProfile view, and changed the router.

---
+++ src/views/UserProfile.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="user-profile">
+    <h1>This is the profile view</h1>
+    <div class="content">
+      Contenido
+       <ShowNotes></ShowNotes>
+    </div>
+  </div>
+</template>
+
+<script>
+import ShowNotes from '@/components/show-notes.vue'
+
+export default {
+  name: 'userprofile',
+  components: {
+    ShowNotes
+  }
+}
+</script>
