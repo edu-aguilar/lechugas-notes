62c75d0b9d884dc9555f05fd8a82bafb2f1730a3
Adrián Fernández <adrianfernandezdiazg@gmail.com>
2019-05-23 11:17:55 +0200

    refactor: deleted the "show-notes" component, and created the "note.js" component, and changed the logic into the UserProfile view.

---
+++ src/components/note.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="note">
+        <p> Note number: {{index+1}} </p>
+        <p> Description: {{notesFormated.description}} </p>
+        <p> Completed: {{notesFormated.completed}} <p>
+        <p> Creation date: {{notesFormated.createdAt}} <p>
+        <p> Last update: {{notesFormated.updatedAt}} </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Note',
+  props: {
+      notesFormated: {
+        description:'',
+        completed: '',
+        createdAt: '',
+        updateAt: ''
+      },
+      index: 0
+  }
+}
+</script>
+
+<style>
+div.note {
+  padding: 5px;
+  margin: 5px;
+  border: solid;
+  min-width: 200px;
+  background-color: wheat;
+}
+
+</style>
--- src/tasks.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-
-export const getNotes = function () {
-  const baseURL = 'http://localhost:3000/tasks'
-  return axios.get(baseURL)
-}
