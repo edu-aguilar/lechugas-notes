a454c2846824774c75988c141486b1bc7fa2eee4
Adrián Fernández <adrianfernandezdiazg@gmail.com>
2019-05-23 18:22:49 +0200

    feature: create-user and login-user dispatch an event to showSpinner in UserProfile.

--- src/components/user-login.vue
+++ src/components/user-login.vue
@@ -43,6 +43,7 @@ export default {
     _printLogingUserMessage (color, text) {
       document.querySelector('.login-message').style.color = color
       document.querySelector('.login-message').textContent = text
+      this.$emit('showSpinner')
     },
     _setAuthToken (token) {
       this.$http.defaults.headers.common['Authorization'] = token
