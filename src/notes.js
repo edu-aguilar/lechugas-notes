c7a77c029a84a596130a6a98e9ce30c633d305db
Adrián Fernández <adrianfernandezdiazg@gmail.com>
2019-05-23 13:04:14 +0200

    created: deleteNote into the notes.js https requests file.

--- src/notes.js
+++ src/notes.js
@@ -4,3 +4,9 @@ export const getNotes = function () {
   const baseURL = 'http://localhost:3000/tasks'
   return axios.get(baseURL)
 }
+
+export const deleteNote = function (key) {
+  const baseURL = `http://localhost:3000/tasks/${key}`
+
+  return axios.delete(baseURL)
+}
