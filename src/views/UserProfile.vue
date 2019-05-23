199e291d82d9a0b9c0a4b898fc9996bbc3f14744
Adrián Fernández <adrianfernandezdiazg@gmail.com>
2019-05-23 09:33:44 +0200

    feature: created the "logout" button and the http request in the user.js file.

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -3,17 +3,29 @@
     <h1>This is the profile view</h1>
     <div class="content">
        <ShowNotes></ShowNotes>
+        <button v-on:click="logOut">Logout</button>
     </div>
   </div>
 </template>
 
 <script>
 import ShowNotes from '@/components/show-notes.vue'
+import { clearSession } from '@/user.js'
 
 export default {
   name: 'userprofile',
   components: {
     ShowNotes
+  },
+  methods: {
+    logOut: function () {
+      clearSession()
+      this._clearAuthToken()
+      this.$router.push('/home')
+    },
+    _clearAuthToken () {
+      this.$http.defaults.headers.common['Authorization'] = null
+    }
   }
 }
 </script>
