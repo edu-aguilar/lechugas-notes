b04edf8a3493f2be0fb0a6369737febdf9240def
Adrián Fernández <adrianfernandezdiazg@gmail.com>
2019-05-23 17:41:10 +0200

    feature: added the getFilteredNotes that receives some optional parameters and makes the https request to the backend.

--- src/notes.js
+++ src/notes.js
@@ -4,3 +4,23 @@ export const getNotes = function () {
   const baseURL = 'http://localhost:3000/tasks'
   return axios.get(baseURL)
 }
+
+export const getFilteredNotes = function (field, completed, description) {
+  const baseURL = 'http://localhost:3000/tasks?limit=16&'
+  const filters = []
+
+  /// tasks?sortBy=createdAt:asc&completed=true
+  if (field !== 'default') {
+    filters.push(`sortBy=${field}`)
+  }
+
+  if (completed !== 'all') {
+    filters.push(`completed=${completed}`)
+  }
+
+  if (description) {
+    filters.push(`description=${description}`)
+  }
+
+  return axios.get(baseURL + filters.join('&'))
+}
